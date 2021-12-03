daa8626d5e0690d6f4a418d1b235eb2ef3b3ab64
mohammedferoz28 <60225262+mohammedferoz28@users.noreply.github.com>
2021-12-03 09:48:26 +0530

    Update App.java
    updated app.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Feroz" );
         System.out.println( "Square root "+Math.sqrt(16) );
     }
 }
